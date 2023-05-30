6c00bb0b5d678efb99b0638052c6207222e673c4
millywanderi <milliwanderi5@gmail.com>
2023-05-30 07:09:52 -0700

    class Square that defines a square by: (based on 5-square.py)

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""This module defines an empty class square"""
+
+
+class Square:
+
+    """A class defining a square"""
+
+    def __init__(self, size=0, position=(0, 0)) -> None:
+        """
+        Instantiates class attributes
+
+        Attributes:
+             size: size of square
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Gets class private attribute to be used"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self, value):
+        """Gets class private attribute to be used"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or value[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[1]) is not int or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """
+        returns the current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """prints square in form of # stdout"""
+        if self.__size == 0:
+            print()
+        else:
+            num = 0
+            while num < self.__size:
+                numeral = 0
+
+                while numeral < self.__size:
+                    print("{}".format("#"), end='')
+                    numeral += 1
+                print()
+                num += 1
