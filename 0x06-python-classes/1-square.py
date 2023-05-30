129d8e0b03f8ee5f32c8b18564a94ba690d3178e
millywanderi <milliwanderi5@gmail.com>
2023-05-30 05:18:05 -0700

    class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module defines an empty class square"""
+
+
+class Square:
+
+    """A class defining a square"""
+
+    def __init__(self, size=0):
+        """
+        Instantiates class attributes
+
+        Attributes:
+             __size: size of square
+        """
+        self.__size = __size
