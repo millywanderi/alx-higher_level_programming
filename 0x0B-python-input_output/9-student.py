1a1432bc9613adce1d1297d8b6b91f02e3e2af99
millywanderi <milliwanderi5@gmail.com>
2023-06-13 04:21:31 -0700

    a class Student that defines a student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This is a student class module"""
+
+
+class Student:
+    """Class defining a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instances class student
+        Arguments:
+            first_name: student's first name
+            last_name: student's last name
+            age: student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def class_to_json(obj):
+        """a function that retrieves dictionary representation"""
+        return self.__dict__
