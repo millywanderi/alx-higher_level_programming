f96afd3a0f34d32acebfd1ae7bdba7349862ff3c
millywanderi <milliwanderi5@gmail.com>
2023-06-13 04:44:50 -0700

    a class Student that defines a student by: (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This is a student class module"""
+
+
+class Student():
+    """Class defining a student"""
+    def __init__(self, first_name, last_name, age):
+        """Instances class student
+        Arguments:
+            first_name: student's first name
+            last_name: student's last name
+            age: student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation of a
+        Student instance (same as 8-class_to_json.py)
+        """
+        key_valid = []
+        dict_return = {}
+        if type(attrs) is list:
+            for ele in attrs:
+                if type(ele) is str and ele in self.__dict__:
+                    dict_return.update({ele: self.__dict__[ele]})
+            return dict_return
+        return self.__dict__
