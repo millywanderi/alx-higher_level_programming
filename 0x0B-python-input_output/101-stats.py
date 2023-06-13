6f02f3d607c84be65779bc08947949f036d56008
millywanderi <milliwanderi5@gmail.com>
2023-06-13 06:17:35 -0700

    a script that reads stdin line by line and computes metrics

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -3,26 +3,36 @@
 
 
 import sys
-code = ['200', '300', '400', '401', '403', '404', '405', '500']
-code_h = []
-size_sum = 0
-m = 0
-try:
-    for line in sys.stdin:
-        toks = line.split(' ')
-        size_sum += int(toks[-1])
-        code_h.append(toks[-2])
-        if m == 9:
-            m = 0
-            print("File size: " + str(size_sum))
-            for cody in code:
-                if cody in code_h:
-                    print('{}: {}'.format(cody, code_h.count(cody)))
-                else:
-                    m += 1
-except KeyboardInterrupt:
-    print('File size: ' + str(size_sum))
-    for cody in code:
-        if cody in code_h:
-            print('{}: {}'.format(cody, code_h.count(cody)))
-    sys.exit()
+
+
+if __name__ == "__main__":
+    siz = [0]
+    code = ['200', '300', '400', '401', '403', '404', '405', '500']
+
+    def look_match(line):
+        try:
+            line = line[:-1]
+            ele = line.split(' ')
+            siz[0] += int(ele[-1])
+            cody = int(ele[-2])
+            if cody in code:
+                code[cody] += 1
+        except Exception as e:
+            pass
+
+    def print_stat():
+        print("File size: {}".format(siz[0]))
+        for m in sorted(code.keys()):
+            if code[m]:
+                print("{}: {}".format(m, code[m]))
+    m = 1
+    try:
+        for line in sys.stdin:
+            look_match(line)
+            if m % 10 == 0:
+                print_stat()
+            m += 1
+    except KeyboardInterrupt:
+        print_stat()
+        raise
+    print_stat()
