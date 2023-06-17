e2284ba33d5749db08fcde6aab8a467fdd05966b
millywanderi <milliwanderi5@gmail.com>
2023-06-17 07:43:17 -0700

    class will be the base of all other classes in this project

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,8 @@
 This module defines a class base
 """
 
+import json
+
 
 class Base:
 
@@ -13,4 +15,28 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id  = Base.__nb_objects
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return "[]"
+        dictionaries = json.dumps(list_dictionaries)
+        return dictionaries
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            list_objs = []
+        saving = []
+        for x in list_objs:
+            saving.append(cls.to_dictionary(x))
+        with open(cls.__name__ + ".json", "w") as file:
+            file.write(cls.to_json_string(saving))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        dic = json.loads(json_string)
+        return dic
