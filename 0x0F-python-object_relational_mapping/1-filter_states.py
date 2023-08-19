991c754112fa3d3bd1ac208bd958515495961b58
millywanderi <milliwanderi5@gmail.com>
2023-08-18 22:57:13 -0700

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,30 @@
+#!usr/bin/python3
+
+"""
+Module that list all states with a name starting
+with N from the database.
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE\
+                LIKE BINARY 'N%' ORDER BY states.id ASC""")
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
