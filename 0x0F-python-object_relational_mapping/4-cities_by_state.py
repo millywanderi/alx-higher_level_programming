a8558753b82f44c4da4c50487f8c71f5433d12a9
millywanderi <milliwanderi5@gmail.com>
2023-08-19 01:24:25 -0700

    a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module that lists all cities from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+        )
+
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON state_id = states.id\
+                ORDER BY cities.id ASC"
+                )
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
